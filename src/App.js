91f9e576b54122638fa3807b791473a182946723
swarnjit <swarnjitchahal@gmail.com>
2020-03-22 11:20:47 -0400

    First commit- pushing files to remote repo

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    strng: [],
+    x: ""
+  };
+
+  async componentDidMount() {
+    fetch("http://localhost:8081/hello")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          strng: data
+        })
+      );
+  }
+  render() {
+    const { strng } = this.state;
+    console.log(strng);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Edit{strng} and save to reload.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
