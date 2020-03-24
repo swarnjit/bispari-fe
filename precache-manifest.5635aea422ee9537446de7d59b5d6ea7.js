dfb4e9e9ce4516222f6050f8bf5d07b4f179e2b9
swarnjit <swarnjitchahal@gmail.com>
2020-03-23 22:07:46 -0400

    Updates

---
+++ precache-manifest.5635aea422ee9537446de7d59b5d6ea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6893b909adb9467d6ab11d307ba72eda",
+    "url": "/bispari-fe/index.html"
+  },
+  {
+    "revision": "144db3c090bcafedabe6",
+    "url": "/bispari-fe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fbfc42b05e16556b68be",
+    "url": "/bispari-fe/static/js/2.05343e12.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/bispari-fe/static/js/2.05343e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144db3c090bcafedabe6",
+    "url": "/bispari-fe/static/js/main.7af44bfd.chunk.js"
+  },
+  {
+    "revision": "b2f4e7e780ce4d3ef665",
+    "url": "/bispari-fe/static/js/runtime-main.33354d69.js"
+  }
+]);
