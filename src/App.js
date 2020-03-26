e0f36df243ae3cd82d022d9518ef358bc6628f20
swarnjit <swarnjitchahal@gmail.com>
2020-03-26 11:21:42 -0400

    minor changes to add custom title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import "./App.css";
-import { AppBar, IconButton, Typography, Button } from "@material-ui/core";
+import {
+  AppBar,
+  IconButton,
+  Typography,
+  Button,
+  MenuItem,
+  Menu
+} from "@material-ui/core";
 import { Toolbar } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
@@ -18,8 +25,16 @@ const useStyles = makeStyles(theme => ({
 }));
 export default function App() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
   return (
-    <div className="App">
+    <React.Fragment className="App">
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -27,15 +42,26 @@ export default function App() {
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
+            onClick={handleClick}
           >
             <MenuIcon />
           </IconButton>
+          <Menu
+            id="simple-menu"
+            keepMounted
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>SSSS</MenuItem>
+            <MenuItem onClick={handleClose}>SSSS</MenuItem>
+          </Menu>
           <Typography variant="h6" className={classes.title}>
             Bispari
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-    </div>
+    </React.Fragment>
   );
 }
